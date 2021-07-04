0001dc33ad093808f1bde8a06201d20dae66171d
derdis14 <78613993+derdis14@users.noreply.github.com>
2021-07-04 18:43:50 +0200

    migrated template to Bootstrap 5, auto-formatted code

--- client/assets/js/catSettings.js
+++ client/assets/js/catSettings.js
@@ -21,7 +21,7 @@ $( document ).ready(function() {
   $('#dnamouth').html(defaultDNA.mouthColor);
   $('#dnaeyes').html(defaultDNA.eyesColor);
   $('#dnaears').html(defaultDNA.earsColor);
-    
+
 //   $('#dnashape').html(defaultDNA.eyesShape)
 //   $('#dnadecoration').html(defaultDNA.decorationPattern)
 //   $('#dnadecorationMid').html(defaultDNA.decorationMidcolor)
