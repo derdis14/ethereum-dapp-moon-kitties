c207481b383b84bda649a890125f3e8e2becce3f
derdis14 <78613993+derdis14@users.noreply.github.com>
2021-07-06 20:13:54 +0200

    auto-formatted with Prettier

--- client/assets/js/catFactory.js
+++ client/assets/js/catFactory.js
@@ -1,7 +1,7 @@
 function outerColor(code) {
-    //This changes the color of the cat
-    const color = parseInt(code);
-    $(`.cat__head,
+  //This changes the color of the cat
+  const color = parseInt(code);
+  $(`.cat__head,
     .cat__chest,
     .cat__ear--right,
     .cat__ear--left,
@@ -9,66 +9,81 @@ function outerColor(code) {
     .cat__paw-right,
     .cat__paw-left_inner,
     .cat__paw-right_inner,
-    .cat__tail`).css("background", `hsl(0,0%,${color}%)`)
+    .cat__tail`).css("background", `hsl(0,0%,${color}%)`);
 
-    $('#outercode').html('code: '+code) //This updates the text of the badge next to the slider
-    $('#dnaouter').html(code) //This updates the DNA text below the cat
+  $("#outercode").html("code: " + code); //This updates the text of the badge next to the slider
+  $("#dnaouter").html(code); //This updates the DNA text below the cat
 }
 
 function innerColor(code) {
-    const color = parseInt(code);
-    $(`.cat__mouth-contour,
+  const color = parseInt(code);
+  $(`.cat__mouth-contour,
     .cat__chest_inner,
     .cat__ear--left-inside,
-    .cat__ear--right-inside`).css('background', `hsl(0,0%,${color}%)`)
+    .cat__ear--right-inside`).css("background", `hsl(0,0%,${color}%)`);
 
-    $('#innercode').html('code: '+code) //This updates the text of the badge next to the slider
-    $('#dnainner').html(code) //This updates the DNA text below the cat
+  $("#innercode").html("code: " + code); //This updates the text of the badge next to the slider
+  $("#dnainner").html(code); //This updates the DNA text below the cat
 }
 
 function eyesColor(code) {
-    //This changes the color of the cat
-    const color = parseInt(code) - 100;
-    $(".cat__eye span").css("background",`hsl(${color}, 100%, 40%)`)
+  //This changes the color of the cat
+  const color = parseInt(code) - 100;
+  $(".cat__eye span").css("background", `hsl(${color}, 100%, 40%)`);
 
-    $('#eyescode').html('code: '+code) //This updates the text of the badge next to the slider
-    $('#dnaeyes').html(code) //This updates the DNA text below the cat
+  $("#eyescode").html("code: " + code); //This updates the text of the badge next to the slider
+  $("#dnaeyes").html(code); //This updates the DNA text below the cat
 }
-
-
 
 //###################################################
 //Functions below will be used later on in the project
 //###################################################
 function eyeVariation(num) {
-
-    $('#dnashape').html(num)
-    switch (num) {
-        case 1:
-            normalEyes()
-            $('#eyeName').html('Basic')
-            break
-    }
+  $("#dnashape").html(num);
+  switch (num) {
+    case 1:
+      normalEyes();
+      $("#eyeName").html("Basic");
+      break;
+  }
 }
 
 function decorationVariation(num) {
-    $('#dnadecoration').html(num)
-    switch (num) {
-        case 1:
-            $('#decorationName').html('Basic')
-            normaldecoration()
-            break
-    }
+  $("#dnadecoration").html(num);
+  switch (num) {
+    case 1:
+      $("#decorationName").html("Basic");
+      normaldecoration();
+      break;
+  }
 }
 
 async function normalEyes() {
-    await $('.cat__eye').find('span').css('border', 'none')
+  await $(".cat__eye").find("span").css("border", "none");
 }
 
 async function normaldecoration() {
-    //Remove all style from other decorations
-    //In this way we can also use normalDecoration() to reset the decoration style
-    $('.cat__head-dots').css({ "transform": "rotate(0deg)", "height": "48px", "width": "14px", "top": "1px", "border-radius": "0 0 50% 50%" })
-    $('.cat__head-dots_first').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
-    $('.cat__head-dots_second').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
+  //Remove all style from other decorations
+  //In this way we can also use normalDecoration() to reset the decoration style
+  $(".cat__head-dots").css({
+    transform: "rotate(0deg)",
+    height: "48px",
+    width: "14px",
+    top: "1px",
+    "border-radius": "0 0 50% 50%",
+  });
+  $(".cat__head-dots_first").css({
+    transform: "rotate(0deg)",
+    height: "35px",
+    width: "14px",
+    top: "1px",
+    "border-radius": "50% 0 50% 50%",
+  });
+  $(".cat__head-dots_second").css({
+    transform: "rotate(0deg)",
+    height: "35px",
+    width: "14px",
+    top: "1px",
+    "border-radius": "0 50% 50% 50%",
+  });
 }
--- client/assets/js/catSettings.js
+++ client/assets/js/catSettings.js
@@ -1,54 +1,54 @@
 var defaultDNA = {
-    //Colors
-    "outerColor" : 20,
-    "innerColor" : 20,
-    "eyesColor" : 300,
-    //Cattributes
-    "eyesShape" : 1,
-    "decorationPattern" : 1,
-    "decorationMidcolor" : 13,
-    "decorationSidescolor" : 13,
-    "animation" :  1,
-    "lastNum" :  1
-    }
+  //Colors
+  outerColor: 20,
+  innerColor: 20,
+  eyesColor: 300,
+  //Cattributes
+  eyesShape: 1,
+  decorationPattern: 1,
+  decorationMidcolor: 13,
+  decorationSidescolor: 13,
+  animation: 1,
+  lastNum: 1,
+};
 
 // when page load
-$( document ).ready(function() {
-  renderCat(defaultDNA)
+$(document).ready(function () {
+  renderCat(defaultDNA);
 });
 
-function getDna(){
-    var dna = ''
-    dna += $('#dnaouter').html()
-    dna += $('#dnainner').html()
-    dna += $('#dnaeyes').html()
+function getDna() {
+  var dna = "";
+  dna += $("#dnaouter").html();
+  dna += $("#dnainner").html();
+  dna += $("#dnaeyes").html();
 
-    dna += $('#dnashape').html()
-    dna += $('#dnadecoration').html()
-    dna += $('#dnadecorationMid').html()
-    dna += $('#dnadecorationSides').html()
-    dna += $('#dnaanimation').html()
-    dna += $('#dnaspecial').html()
+  dna += $("#dnashape").html();
+  dna += $("#dnadecoration").html();
+  dna += $("#dnadecorationMid").html();
+  dna += $("#dnadecorationSides").html();
+  dna += $("#dnaanimation").html();
+  dna += $("#dnaspecial").html();
 
-    return parseInt(dna)
+  return parseInt(dna);
 }
 
-function renderCat(dna){
-    $('#outercolor').val(dna.outerColor).change()
-    $('#innercolor').val(dna.innerColor).change()
-    $('#eyescolor').val(dna.eyesColor).change()
+function renderCat(dna) {
+  $("#outercolor").val(dna.outerColor).change();
+  $("#innercolor").val(dna.innerColor).change();
+  $("#eyescolor").val(dna.eyesColor).change();
 }
 
 // Changing cat colors
-$('#outercolor').change(()=>{
-    var val = $('#outercolor').val()
-    outerColor(val)
-})
-$('#innercolor').change(()=>{
-  var val = $('#innercolor').val()
-  innerColor(val)
-})
-$('#eyescolor').change(()=>{
-  var val = $('#eyescolor').val()
-  eyesColor(val)
-})
+$("#outercolor").change(() => {
+  var val = $("#outercolor").val();
+  outerColor(val);
+});
+$("#innercolor").change(() => {
+  var val = $("#innercolor").val();
+  innerColor(val);
+});
+$("#eyescolor").change(() => {
+  var val = $("#eyescolor").val();
+  eyesColor(val);
+});
