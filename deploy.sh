e068c49e5917ab965ce253421462d5228802d462
derdis14 <78613993+derdis14@users.noreply.github.com>
2021-08-18 15:19:02 +0200

    added script for frontend deployment

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# navigate into the build output directory
+cd client
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+# create a new git repository
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:derdis14/ethereum-dapp-crypto-kitties.git master:gh-pages
+
+# delete the new git repository
+rm -r -f .git
+
+# jump back to last location
+cd -
